881156909b3e70ec6554504d4b2f38d95829f77a
Chris Moeketsi <chrismoeketsi@gmail.com>
2025-11-22 21:12:06 +0200

    yvbuhnij

--- src/components/UI/Footer/index.tsx
+++ src/components/UI/Footer/index.tsx
@@ -75,7 +75,7 @@ const Footer = () => {
           </FooterMiddle>
           <FooterBottom>
             <Translator>
-              <h3>English (United Kingdom)</h3>
+              <h3>English (South Africa)</h3>
               <Image src={ic_chevron_down} alt="chevron down" />
             </Translator>
             <CopyRight>
